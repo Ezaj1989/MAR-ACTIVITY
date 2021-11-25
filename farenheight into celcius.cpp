07efe0b8739182176083f20e1e764064c9ed3bb3
Ezaj ali <86915622+Ezaj1989@users.noreply.github.com>
2021-11-25 20:51:50 +0530

    Add files via upload

---
+++ farenheight into celcius.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+	float A,r;
+	
+	printf("\n\n area of the circle\n\n");
+	
+	printf("\n enter the value of r");
+	scanf("%f",&r);
+	A =3.14*r*r;
+	printf("%f",A );
+	return 0;
+	
+	
+	
+}
